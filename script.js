f28010c7d2d17eebca8eeb0480bd644494488d81
noreasoninternet <garena100acc@gmail.com>
2025-02-19 15:50:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const container = document.querySelector(".container");
-const toggleBtn = document.getElementById("toggleBtn");
-const showRegister = document.getElementById("showRegister");
-const showLogin = document.getElementById("showLogin");
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
 
-showRegister.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
-    toggleBtn.innerText = "Login";
-    document.getElementById("toggleTitle").innerText = "Welcome Back!";
-    document.getElementById("toggleText").innerText = "Already have an account?";
-});
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+    });
 
-showLogin.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
-    toggleBtn.innerText = "Register";
-    document.getElementById("toggleTitle").innerText = "Hello, Welcome!";
-    document.getElementById("toggleText").innerText = "Don't have an account?";
-});
-
-toggleBtn.addEventListener("click", () => {
-    container.classList.toggle("right-panel-active");
-});
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+    });
